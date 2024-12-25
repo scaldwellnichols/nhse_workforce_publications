bb2451f5f3026748fefd5706c5a1064a933002ba
Scott Caldwell-Nichols <scaldwellnichols@Scotts-MacBook-Pro.local>
2024-12-25 13:01:48 +0000

    parameters file for repo containing URLs etc

---
+++ src/parameters.py
@@ -0,0 +1,2 @@
+workforce_statistics_url = 'https://digital.nhs.uk/data-and-information/publications/statistical/nhs-workforce-statistics'
+gp_workforce_statistics_url = 'https://digital.nhs.uk/data-and-information/publications/statistical/general-and-personal-medical-services'

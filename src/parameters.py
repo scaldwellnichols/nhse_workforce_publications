e39d10195f651adafb24ffea3ada55831ff2800f
Scott Caldwell-Nichols <scaldwellnichols@Scotts-MacBook-Pro.local>
2025-03-16 16:18:39 +0000

    sets up data file structure

--- src/parameters.py
+++ src/parameters.py
@@ -1,2 +1,9 @@
-workforce_statistics_url = 'https://digital.nhs.uk/data-and-information/publications/statistical/nhs-workforce-statistics'
-gp_workforce_statistics_url = 'https://digital.nhs.uk/data-and-information/publications/statistical/general-and-personal-medical-services'
+import pathlib as Path
+
+BASE_DIR = Path.Path('.')
+DATA_DIR = BASE_DIR / 'data'
+OUTPUT_DIR = BASE_DIR / 'output'
+
+nhs_digital_url = 'https://digital.nhs.uk'
+workforce_statistics_url = f'{nhs_digital_url}/data-and-information/publications/statistical/nhs-workforce-statistics'
+gp_workforce_statistics_url = F'{nhs_digital_url}/data-and-information/publications/statistical/general-and-personal-medical-services'

934e8e2ea3e916f4789b6914b6a579aceca9a6e3
Scott Caldwell-Nichols <scaldwellnichols@Scotts-MacBook-Pro.local>
2025-03-16 20:35:50 +0000

    adds current time period parameter

--- src/parameters.py
+++ src/parameters.py
@@ -4,6 +4,8 @@ BASE_DIR = Path.Path('.')
 DATA_DIR = BASE_DIR / 'data'
 OUTPUT_DIR = BASE_DIR / 'output'
 
+current_time_period = '2024-11'
+
 nhs_digital_url = 'https://digital.nhs.uk'
 workforce_statistics_url = f'{nhs_digital_url}/data-and-information/publications/statistical/nhs-workforce-statistics'
 gp_workforce_statistics_url = F'{nhs_digital_url}/data-and-information/publications/statistical/general-and-personal-medical-services'
